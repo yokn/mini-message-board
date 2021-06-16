fb13479e682f83ea463b8d4289a84680ee3b46b3
yokn <yokn@protonmail.com>
2021-06-16 18:43:05 +0300

    Add form for creating new messages

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: "New Message"});
+});
+
+router.post('/new', function(req, res, next) {
+ const messageText = req.body.messageText;
+ const messageUser = req.body.messageUser;
+  
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+
+  res.redirect('/')
+});
+
 module.exports = router;
