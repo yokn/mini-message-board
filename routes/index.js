87b3df469763a0e50b3093a85d0b9bc99530cef5
yokn <yokn@protonmail.com>
2021-06-16 18:12:12 +0300

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+module.exports = router;
